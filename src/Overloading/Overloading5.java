d582a5d5071c07dda7b445b55ae87e7032c9cae2
Datta <datsayan@amazon.com>
2024-01-31 16:36:44 +0530

    updated Overloading5

--- src/Overloading/Overloading5.java
+++ src/Overloading/Overloading5.java
@@ -13,5 +13,7 @@ public class Overloading5 {
         obj.m1(10, 10.5f);// will call the 1st method
         obj.m1(10.5f,10);//will call the 2nd method
         // Both above methods are overloaded menthod as agruements are same type but order is different. 
+        //obj.m1(10,10);// here as int can be promoted to float both the method can be call, Hence compiler can't decide which one to call. So, The method m1(int, float) is ambiguous for the type Overloading5
+        obj.m1(10.5f, 10.5f);// float can't be demoted to int so none of the method signature is matching. Hence, The method m1(float, int) in the type Overloading5 is not applicable for the arguments (float, float)
     }
 }
